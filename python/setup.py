df47f74fee09e9cf74af878177a1d8f469d9ba32
Alex Poms <alexpoms@gmail.com>
2018-11-07 15:42:12 -0800

    Bump version

--- python/setup.py
+++ python/setup.py
@@ -35,7 +35,7 @@ module1 = Extension(
 
 setup(
     name='hwang',
-    version='0.3.5',
+    version='0.3.6',
     url='https://github.com/scanner-research/hwang',
     author='Alex Poms',
     author_email='apoms@cs.cmu.edu',
